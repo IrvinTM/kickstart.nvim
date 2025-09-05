bc5a709a2be3c2cb296794ae54bc59478db1a7c6
IrvinTM <tm22012@ues.edu.sv>
2025-09-04 19:00:02 -0600

    modified some things

--- init.lua
+++ init.lua
@@ -1,91 +1,3 @@
---[[
---
---Marked custom changes with -- custom confi
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-========                                    .-----.          ========
-========         .----------------------.   | === |          ========
-========         |.-""""""""""""""""""-.|   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||   KICKSTART.NVIM   ||   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||                    ||   |-----|          ========
-========         ||:Tutor              ||   |:::::|          ========
-========         |'-..................-'|   |____o|          ========
-========         `"")----------------(""`   ___________      ========
-========        /::::::::::|  |::::::::::\  \ no mouse \     ========
-========       /:::========|  |==hjkl==:::\  \ required \    ========
-========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
-========                                                     ========
-=====================================================================
-=====================================================================
-
-What is Kickstart?
-
-  Kickstart.nvim is *not* a distribution.
-
-  Kickstart.nvim is a starting point for your own configuration.
-    The goal is that you can read every line of code, top-to-bottom, understand
-    what your configuration is doing, and modify it to suit your needs.
-
-    Once you've done that, you can start exploring, configuring and tinkering to
-    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
-    or immediately breaking it into modular pieces. It's up to you!
-
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
-
-    After understanding a bit more about Lua, you can use `:help lua-guide` as a
-    reference for how Neovim integrates Lua.
-    - :help lua-guide
-    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
-
-Kickstart Guide:
-
-  TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
-
-    If you don't know what this means, type the following:
-      - <escape key>
-      - :
-      - Tutor
-      - <enter key>
-
-    (If you already know the Neovim basics, you can skip this step.)
-
-  Once you've completed that, you can continue working through **AND READING** the rest
-  of the kickstart init.lua.
-
-  Next, run AND READ `:help`.
-    This will open up a help window with some basic information
-    about reading, navigating and searching the builtin help documentation.
-
-    This should be the first place you go to look when you're stuck or confused
-    with something. It's one of my favorite Neovim features.
-
-    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
-    which is very useful when you're not exactly sure of what you're looking for.
-
-  I have left several `:help X` comments throughout the init.lua
-    These are hints about where to find more information about the relevant settings,
-    plugins or Neovim features used in Kickstart.
-
-   NOTE: Look for lines like this
-
-    Throughout the file. These are for you, the reader, to help you understand what is happening.
-    Feel free to delete them once you know what you're doing, but they should serve as a guide
-    for when you are first encountering a few different constructs in your Neovim config.
-
-If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now! :)
---]]
-
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
@@ -93,10 +5,9 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 --my custom keymaps
-vim.keymap.set('n', '<leader><Tab>', '<C-6>', { noremap = true, silent = true }) -- map leader v to select all doing ggVG
 
--- Set up indentation for Java files
-vim.api.nvim_create_autocmd('FileType', {
+-- Set up indentation for Java files not working will check later
+--[[ vim.api.nvim_create_autocmd('FileType', {
   pattern = 'java',
   callback = function()
     vim.opt_local.tabstop = 4 -- Number of spaces a tab counts for
@@ -104,7 +15,9 @@ vim.api.nvim_create_autocmd('FileType', {
     vim.opt_local.expandtab = true -- Use spaces instead of tabs
     vim.opt_local.autoindent = true -- Copy indent from current line when starting a new line
   end,
-})
+}) ]]
+
+-- =========================== Open and close the terminal using leader enter =====================================================
 -- _____________________________________
 -- Variable to store the terminal buffer ID
 local term_buf = nil
@@ -138,14 +51,25 @@ end
 
 -- Map <leader><Enter> to toggle the terminal
 vim.keymap.set('n', '<leader><CR>', toggle_terminal, { noremap = true, silent = true })
--- ______________________________________
+
+-- <leader><v> to select all
+--
 vim.keymap.set('n', '<leader>v', 'ggVG', { desc = 'Copy all' })
+
 -- map leader d to delete line without copying it to clipboard using _ register
 vim.keymap.set('n', '<leader>d', '"_dd', { desc = 'Delete line without copying' })
+
 -- map leader e to go to the end of the line using $
 vim.keymap.set('n', '<leader>e', '$', { desc = 'Go to end of line' })
+
+-- leader a to replace all
 vim.keymap.set('n', '<leader>a', ':%s/\\<<C-r><C-w>\\>//g<left><left>', { desc = 'Replace all' })
--- map leader enter to open and close the terminal
+
+-- =========================== END   map leader enter to open and close the terminal ====================
+--
+--
+--
+--
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
@@ -203,7 +127,7 @@ vim.opt.splitbelow = true
 --  See `:help 'list'`
 --  and `:help 'listchars'`
 vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.listchars = { tab = '  »', trail = '·', nbsp = '␣' }
 
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = 'split'
@@ -230,7 +154,7 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
@@ -365,7 +289,7 @@ require('lazy').setup({
   --  config = function() ... end
   --  custom config
   --custom markdown added plugin
-  {
+  --[[ {
     'iamcco/markdown-preview.nvim',
     cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
     build = 'cd app && yarn install',
@@ -373,7 +297,8 @@ require('lazy').setup({
       vim.g.mkdp_filetypes = { 'markdown' }
     end,
     ft = { 'markdown' },
-  },
+  },  ]]
+
   --  Eslint custom added config
   'jose-elias-alvarez/null-ls.nvim',
   config = function()
@@ -740,8 +665,8 @@ require('lazy').setup({
         -- clangd = {},
         --
         jdtls = {},
-        gopls = {},
-        -- pyright = {},
+        -- gopls = {},
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -749,7 +674,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        ts_ls = {},
         --
 
         lua_ls = {
@@ -892,6 +817,7 @@ require('lazy').setup({
         },
         completion = { completeopt = 'menu,menuone,noinsert' },
 
+        -- MAPPINGS FOR AUTOCOMPLETE
         -- For an understanding of why these mappings were
         -- chosen, you will need to read `:help ins-completion`
         --
